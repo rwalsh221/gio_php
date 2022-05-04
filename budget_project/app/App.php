0e41f00a3292e60d49d4e00a398ca8532da48ddd
Richard Walsh <richwalsh2@googlemail.com>
2022-05-04 18:09:14 +0100

    1.32 first part of project complete

--- budget_project/app/App.php
+++ budget_project/app/App.php
@@ -2,4 +2,69 @@
 
 declare(strict_types = 1);
 
-// Your Code
+function getTransactionFiles(string $dirPath): array {
+    $files = [];
+
+    foreach(scandir($dirPath) as $file) {
+        if (is_dir($file)) {
+            continue;
+        }
+
+        $files[] = $dirPath . $file;
+    }
+
+    return $files;
+}
+
+function getTransactions(string $fileName, callable $transactionHandler = null): array {
+    if (! file_exists($fileName)) {
+        trigger_error('File "' . $fileName . '" does not exist', E_USER_ERROR);
+    }
+
+    $file = fopen($fileName, 'r');
+
+    fgetcsv($file); // skips first line of file.
+
+    $transactions = [];
+
+    while (($transaction = fgetcsv($file)) !== false) {
+        if ($transactionHandler !== null) {
+            $transactions[] = $transactionHandler($transaction);
+        } else {
+            $transactions[] = extractTransaction($transaction);
+        }
+
+        
+    }
+
+    return $transactions;
+}
+
+function extractTransaction(array $transactionRow): array {
+    [$date, $checkNumber, $description, $amount] = $transactionRow;
+
+    $amount = (float) str_replace(['$', ','], '', $amount);
+
+    return [
+        'date' => $date,
+        'checkNumber' => $checkNumber,
+        'description' => $description,
+        'amount' =>$amount,
+    ];
+}
+
+function calculateTotals(array $transactions): array {
+    $totals = ['netTotal' => 0, 'totalIncome' => 0, 'totalExpense' => 0];
+
+    foreach($transactions as $transaction) {
+        $totals['netTotal'] += $transaction['amount'];
+
+        if ($transaction['amount'] >= 0) {
+            $totals['totalIncome'] += $transaction['amount'];
+        } else {
+            $totals['totalExpense'] += $transaction['amount'];
+        }
+    }
+
+    return $totals;
+}
